2b6c35762d1574e8ab4e87dc0bbe74423ac708b6
YY-oops <personal_mail_yuan@163.com>
2020-11-26 19:38:48 +0800

    version 6

---
+++ src/test/java/com/nowcoder/community/SensitiveTests.java
@@ -0,0 +1,31 @@
+package com.nowcoder.community;
+
+
+import com.nowcoder.community.util.SensitiveFilter;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.lang.annotation.Target;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter(){
+        String text = "这里可以赌博，可以嫖娼，可以😀吸😀😀毒😀，哈哈哈";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+
+    }
+
+
+}
