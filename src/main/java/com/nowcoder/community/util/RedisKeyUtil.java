2eb0560ff9ddce0a64e4194345b5a36f60349050
YY-oops <personal_mail_yuan@163.com>
2020-11-30 21:55:03 +0800

    version 9

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -12,6 +12,12 @@ public class RedisKeyUtil {
 
     private static final String PREFIX_FLLOWER = "follower";
 
+    private static final String PREFIX_KAPTCHA = "kaptcha";
+
+    private static final String PREFIX_TICKET = "ticket";
+
+    private static final String PREFIX_USER = "user";
+
     // 生成某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
     public static String getEntityLkieKey(int entityType, int entityId){
@@ -34,5 +40,20 @@ public class RedisKeyUtil {
     //follower:entityType:entityId -> zset(userId, now)
     public static String getFollowerKey(int entityType, int entityId) {
         return PREFIX_FLLOWER + SPLIT + entityType + SPLIT + entityId;
+    }
+
+    // 登录验证码
+    public static String getKaptchaKey(String owner) {
+        return PREFIX_KAPTCHA + SPLIT + owner;
+    }
+
+    // 登录的凭证
+    public static String getTicketKey(String ticket) {
+        return PREFIX_TICKET + SPLIT + ticket;
+    }
+
+    // 用户
+    public static String getUserKey(int userId){
+        return PREFIX_USER + SPLIT + userId;
     }
 }
