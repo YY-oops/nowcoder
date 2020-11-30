bbcea6875048ac7c031f36ca42c39eb627e2af76
YY-oops <personal_mail_yuan@163.com>
2020-11-30 10:18:27 +0800

    version 8

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -8,6 +8,10 @@ public class RedisKeyUtil {
 
     private static final String PREFIX_USER_LIKE = "like:user";
 
+    private static final String PREFIX_FLLOWEE = "followee";
+
+    private static final String PREFIX_FLLOWER = "follower";
+
     // 生成某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
     public static String getEntityLkieKey(int entityType, int entityId){
@@ -20,4 +24,15 @@ public class RedisKeyUtil {
         return PREFIX_USER_LIKE + SPLIT + userId;
     }
 
+    // 某个用户关注的实体
+    // followee:userId:entityType -> zset(entityId, now)
+    public static String getFolloweeKey(int userId, int entityType){
+        return PREFIX_FLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // 某个用户拥有的粉丝
+    //follower:entityType:entityId -> zset(userId, now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FLLOWER + SPLIT + entityType + SPLIT + entityId;
+    }
 }
